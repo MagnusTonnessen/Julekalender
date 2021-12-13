2f51a27bc2154476ebccc31f4f4e6887e413112a
Magnus <magnus.tonnessen97@gmail.com>
2021-12-13 11:31:43 +0100

    The spaghetti is fresh

--- src/AdventOfCode/Jul2021/Day8/Day8.kt
+++ src/AdventOfCode/Jul2021/Day8/Day8.kt
@@ -11,18 +11,17 @@ fun main() {
                 .sumOf { it.count { c -> c.length == 2 || c.length == 3 || c.length == 4 || c.length == 7 } }
     )
 
-    println("Part two: " +
-            File("src/AdventOfCode/Jul2021/Day8/Input")
-                .readLines()
-                .map { it.split(" | ") }
-                .sumOf { line ->
-                    val rule = line[0].split(" ")
-                    val code = line[1].split(" ")
-                    val map = mutableMapOf<Int, Set<Char>>()
-                    map[1] = rule.first { it.length == 2 }.toSet()
-                    map[4] = rule.first { it.length == 4 }.toSet()
-                    map[7] = rule.first { it.length == 3 }.toSet()
-                    map[8] = rule.first { it.length == 7 }.toSet()
+    println("Part two: " + File("src/AdventOfCode/Jul2021/Day8/Input")
+        .readLines()
+        .map { it.split(" | ") }
+        .sumOf { line ->
+            val rule = line[0].split(" ")
+            val code = line[1].split(" ")
+            val map = mutableMapOf<Int, Set<Char>>()
+            map[1] = rule.first { it.length == 2 }.toSet()
+            map[4] = rule.first { it.length == 4 }.toSet()
+            map[7] = rule.first { it.length == 3 }.toSet()
+            map[8] = rule.first { it.length == 7 }.toSet()
                     map[3] = rule.first { it.length == 5 && map[1]!!.all { c -> c in it } }.toSet()
                     map[9] = rule.first { it.length == 6 && map[4]!!.all { c -> c in it } }.toSet()
                     map[0] =
