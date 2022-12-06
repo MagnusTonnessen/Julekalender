15255db1b753e6022e46440a2f2ed8cab02bc523
Magnus <magnus.tonnessen97gmail.com>
2022-12-06 11:03:31 +0100

    The Julekalender is fresh

--- src/AdventOfCode/Jul2022/Day5/Day5.kt
+++ src/AdventOfCode/Jul2022/Day5/Day5.kt
@@ -2,43 +2,30 @@ package AdventOfCode.Jul2022.Day5
 
 import java.io.File
 
-var gridPart1 = Array(9) { mutableListOf<Char>() }
-var gridPart2 = Array(9) { mutableListOf<Char>() }
 fun main() {
+    val gridPart1 = Array(9) { mutableListOf<Char>() }
+    val gridPart2 = Array(9) { mutableListOf<Char>() }
     val input = File("src/AdventOfCode/Jul2022/Day5/Input").readLines()
     input.take(8).forEach {
         for (index in it.indices step 4) {
             val char = it.elementAt(index + 1)
             if (char.isLetter()) {
-                gridPart1[index / 4].add(char)
-                gridPart2[index / 4].add(char)
+                gridPart1[index / 4].add(0, char)
+                gridPart2[index / 4].add(0, char)
             }
         }
     }
 
     input.drop(10).forEach {
-        it.filter { char -> char.isDigit() }
-            .split("")
-            .map { num -> num.toInt() }
-            .also { op ->
-                movePart1(op[0], op[1] - 1, op[2] - 1)
-                movePart2(op[0], op[1] - 1, op[2] - 1)
-            }
+        it.split(" ")
+          .filter { str -> str.first().isDigit() }
+          .map { num -> num.toInt() }
+          .also { op ->
+              (0 until op[0]).map { gridPart1[op[1] - 1].removeLast() }.forEach { num -> gridPart1[op[2] - 1].add(num) }
+              (0 until op[0]).map { gridPart2[op[1] - 1].removeLast() }.reversed().forEach { num -> gridPart2[op[2] - 1].add(num) }
+          }
     }
 
     (0 until 9).map { gridPart1[it].last() }.also { println("Part one: ${it.joinToString("")}") }
     (0 until 9).map { gridPart2[it].last() }.also { println("Part two: ${it.joinToString("")}") }
-}
-
-fun movePart1(num: Int, fromCol: Int, toCol: Int) {
-    (0 until num)
-        .map { gridPart1[fromCol].removeLast() }
-        .forEach { gridPart1[toCol].add(it) }
-}
-
-fun movePart2(num: Int, fromCol: Int, toCol: Int) {
-    (0 until num)
-        .map { gridPart2[fromCol].removeLast() }
-        .reversed()
-        .forEach { gridPart2[toCol].add(it) }
 }
