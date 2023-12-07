d2731ae6b344676bd33bc6097a7167eb776483b1
Magnus Tønnessen <magnustonnessen@gmail.com>
2023-12-07 10:20:24 +0100

    The spaghetti is fresh

---
+++ src/AdventOfCode/Jul2020/Day24/Day24.java
@@ -0,0 +1,15 @@
+package AdventOfCode.Jul2020.Day24;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Day24 {
+    public static void main(String[] args) {
+        try {
+            File file = new File("src\\AdventOfCode\\Day14\\Input");
+            Scanner sc = new Scanner(file);
+        } catch (Exception e) {
+            System.out.println("Fuck Java");
+        }
+    }
+}
---
+++ src/AdventOfCode/Jul2020/Day6/Day6.java
@@ -0,0 +1,59 @@
+package AdventOfCode.Jul2020.Day6;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Day6 {
+    public static void main(String[] args) {
+        try {
+            File file = new File("src\\AdventOfCode\\Day6\\Input");
+            Scanner sc = new Scanner(file);
+
+            HashMap<Integer, Integer> groupCount = new HashMap<>();
+            groupCount.put(0, 0);
+
+            HashMap<Integer, HashMap<Character, Integer>> answers = new HashMap<>();
+            answers.put(0, new HashMap<>());
+
+            final AtomicInteger groupID = new AtomicInteger(0);
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
+
+                if (line.equals("")) {
+                    answers.put(groupID.incrementAndGet(), new HashMap<>());
+                    continue;
+                }
+
+                groupCount.merge(groupID.get(), 1, Integer::sum);
+                line.chars().mapToObj(c -> (char) c).forEach(
+                        c -> answers.get(groupID.get()).put(c, answers.get(groupID.get()).merge(c, 1, Integer::sum))
+                );
+            }
+
+            System.out.println("Part one: " +
+                    answers
+                            .values()
+                            .stream()
+                            .mapToInt(HashMap::size)
+                            .sum()
+            );
+
+            System.out.println("Part two: " +
+                    answers
+                            .entrySet()
+                            .stream()
+                            .mapToLong(
+                                    e -> e.getValue()
+                                            .values()
+                                            .stream()
+                                            .filter(i -> i.equals(groupCount.get(e.getKey())))
+                                            .count())
+                            .sum()
+            );
+        } catch (Exception ignored) {
+            System.out.println("Fuck Java");
+        }
+    }
+}
