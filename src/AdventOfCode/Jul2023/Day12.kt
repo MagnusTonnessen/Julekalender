29ac7e6c37679bdc4c7271b7ee74acc3a3f392fb
Magnus Tønnessen <magnustonnessen@gmail.com>
2023-12-19 10:18:47 +0100

    The spaghetti is fresh

--- src/AdventOfCode/Jul2023/Day12.kt
+++ src/AdventOfCode/Jul2023/Day12.kt
@@ -1,7 +1,66 @@
 package AdventOfCode.Jul2023
 
 import java.io.File
+import java.util.concurrent.atomic.AtomicInteger
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.runBlocking
+
+val progressCount = AtomicInteger(0)
 
 fun main() {
-
+    File("src/Input")
+        .readLines()
+        .map { it.split(" ") }
+        .map { Pair(it.first(), it.last().split(",").map { it.toInt() }) }
+        .run {
+            println(
+                "Part one: ${sumOf { (line, pattern) -> generateCombinations(line, pattern).count { it.isValid(pattern) } }}"
+            )
+            // Holy fudge, amazingly bad solution, but cool progress bar though
+            runBlocking {
+                coroutineScope {
+                    map { (line, pattern) ->
+                        async(Dispatchers.Default) {
+                            generateCombinations(
+                                List(5) { line }.joinToString("?"),
+                                List(5) { pattern }.flatten()
+                            ).count { it.isValid(List(5) { pattern }.flatten()) }.also {
+                                progressCount.incrementAndGet().also {
+                                    System.out.flush()
+                                    print("\r▕${"█".repeat(it * 100 / size)}${" ".repeat(100 - it * 100 / size)}▏ ${it.toFloat().div(size).times(100)} %")
+                                }
+                            }
+                        }
+                    }
+                    .sumOf { it.await() }
+                    .let { println("Part two: $it") }
+                }
+            }
+        }
 }
+
+private fun String.isValid(pattern: List<Int>): Boolean =
+    !contains('?') && trim('.').split("\\.+".toRegex()).map { it.length } == pattern
+
+private fun String.isPartlyValid(pattern: List<Int>): Boolean =
+    takeWhile { it != '?' }.dropLastWhile { it != '.' }.trim('.').let {
+        it.isEmpty() || it.split("\\.+".toRegex()).map { it.length }.zip(pattern)
+            .all { (a, b) -> a == b }
+    }
+
+private fun generateCombinations(
+    input: String,
+    pattern: List<Int>,
+    index: Int = 0,
+    current: String = ""
+): List<String> =
+    when {
+        !current.isPartlyValid(pattern) -> emptyList()
+        index == input.length           -> listOf(current)
+        input[index] == '?'             -> generateCombinations(input, pattern, index + 1, "$current.") +
+                generateCombinations(input, pattern, index + 1, "$current#")
+
+        else                            -> generateCombinations(input, pattern, index + 1, current + input[index])
+    }
--- src/AdventOfCode/Jul2023/Day13.kt
+++ src/AdventOfCode/Jul2023/Day13.kt
@@ -1,7 +1,55 @@
 package AdventOfCode.Jul2023
 
 import java.io.File
+import kotlin.math.min
 
 fun main() {
-
+    File("src/Input")
+        .readText()
+        .split("\n\n")
+        .map { it.lines().map { it.toList() } }
+        .run {
+            println("Part one: ${sumOf { it.findMirror()!! }}")
+            println("Part two: ${sumOf { it.findMirrorWithSmudge() }}")
+        }
 }
+
+private fun List<List<Char>>.findMirrorWithSmudge(): Int {
+    val mirror = findMirror()!!
+    for (y in indices) {
+        for (x in get(0).indices) {
+            return mapIndexed { i, row ->
+                if (i == y)
+                    row.mapIndexed { j, c ->
+                        when {
+                            j == x && c == '#' -> '.'
+                            j == x && c == '.' -> '#'
+                            else               -> c
+                        }
+                    }
+                else row
+            }.findMirror(mirror) ?: continue
+        }
+    }
+    throw Exception("No mirror found")
+}
+
+private fun List<List<Char>>.findMirror(skip: Int = -1): Int? =
+    indices.drop(1).find { if (it == skip / 100) false else isMirrorRow(it) }?.times(100) ?:
+        get(0).indices.drop(1).find { if (it == skip) false else isMirrorColumn(it) }
+
+private fun List<List<Char>>.isMirrorColumn(col: Int): Boolean =
+    min(col, get(0).size - col)
+        .let { width ->
+            indices.none { r ->
+                get(r).subList(col - width, col) != get(r).subList(col, col + width).reversed()
+            }
+        }
+
+private fun List<List<Char>>.isMirrorRow(row: Int): Boolean =
+    min(row, size - row)
+        .let { width ->
+            get(0).indices.none { c ->
+                map { it[c] }.subList(row - width, row) != map { it[c] }.subList(row, row + width).reversed()
+            }
+        }
