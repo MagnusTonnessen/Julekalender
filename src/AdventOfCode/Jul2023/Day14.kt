4943c72e7cdfc36506fee10656ff5fb59bd9bf4c
Magnus Tønnessen <magnustonnessen@gmail.com>
2023-12-21 09:54:08 +0100

    The spaghetti is fresh

--- src/AdventOfCode/Jul2023/Day14.kt
+++ src/AdventOfCode/Jul2023/Day14.kt
@@ -8,81 +8,64 @@ fun main() {
         .map { it.toList() }
         .run {
             println("Part one: ${tilt('N').totalLoad()}")
-            println("Part two: ${cycle(1000000).totalLoad()}")
+            println("Part two: ${cycle(1000000000)}")
         }
 }
 
 private fun List<List<Char>>.totalLoad(): Int =
     reversed().mapIndexed { i, row -> (i + 1) * row.count { it == 'O' } }.sum()
 
-private fun List<List<Char>>.cycle(n: Int): List<List<Char>> {
-    var count = 0
+private fun List<List<Char>>.cycle(n: Int): Int {
     var tilted = this
-    do {
-        val (changed, next) = Pair(false, tilted).tilt('N').tilt('W').tilt('S').tilt('E')
-        tilted = next
-    } while (changed && ++count < n)
-    return tilted
+    val loads = mutableListOf(totalLoad())
+    repeat(n) {
+        tilted = tilted.tilt('N').tilt('W').tilt('S').tilt('E')
+        loads.apply { add(tilted.totalLoad()) }.findPattern()?.let { (startIndex, pattern) ->
+            return loads[startIndex + (n - startIndex) % pattern.size]
+        }
+    }
+    return tilted.totalLoad()
 }
 
-private fun List<List<Char>>.tilt(dir: Char): List<List<Char>> =
-    Pair(false, this).tilt(dir).second
+fun List<Int>.findPattern(): Pair<Int, List<Int>>? {
+    for (startIndex in 0 until size / 2) {
+        for (patternSize in 1 until (size - startIndex) / 2) {
+            val pattern = subList(startIndex, startIndex + patternSize)
+            if (drop(startIndex).windowed(patternSize, patternSize).all { it == pattern }) {
+                return Pair(startIndex, pattern)
+            }
+        }
+    }
+    return null
+}
 
-private fun Pair<Boolean, List<List<Char>>>.tilt(dir: Char): Pair<Boolean, List<List<Char>>> {
-    var changed = first
-    val tilted = second.toMutableList().map { it.toMutableList() }
-    second.indices.forEach { y ->
-        second.indices.forEach { x ->
-            val char = second.get(y)[x]
-            if (char == 'O') {
-                when (dir) {
-                    'N' -> {
-                        for (i in y downTo 1) {
-                            if (tilted[i - 1][x] == '.') {
-                                tilted[i - 1][x] = 'O'
-                                tilted[i][x] = '.'
-                                changed = true
-                            } else {
-                                break
-                            }
-                        }
-                    }
-                    'W' -> {
-                        for (i in x downTo 1) {
-                            if (tilted[y][i - 1] == '.') {
-                                tilted[y][i - 1] = 'O'
-                                tilted[y][i] = '.'
-                                changed = true
-                            } else {
-                                break
-                            }
-                        }
-                    }
-                    'S' -> {
-                        for (i in y until second.lastIndex) {
-                            if (tilted[i + 1][x] == '.') {
-                                tilted[i + 1][x] = 'O'
-                                tilted[i][x] = '.'
-                                changed = true
-                            } else {
-                                break
-                            }
-                        }
-                    }
-                    'E' -> {
-                        for (i in x until second.lastIndex) {
-                            if (tilted[y][i + 1] == '.') {
-                                tilted[y][i + 1] = 'O'
-                                tilted[y][i] = '.'
-                                changed = true
-                            } else {
-                                break
-                            }
-                        }
+private fun List<List<Char>>.tilt(dir: Char): List<List<Char>> {
+    val tilted = toMutableList().map { it.toMutableList() }
+    val yIndices = if (dir == 'N') indices else indices.reversed()
+    val xIndices = if (dir == 'W') indices else indices.reversed()
+    yIndices.forEach { y ->
+        xIndices.forEach { x ->
+            if (tilted[y][x] == 'O') {
+                val range = when (dir) {
+                    'N' -> y downTo  1
+                    'W' -> x downTo  1
+                    'S' -> y until lastIndex
+                    else -> x until lastIndex
+                }
+                for (i in range) {
+                    val currY = if (dir in listOf('N', 'S')) i else y
+                    val currX = if (dir in listOf('W', 'E')) i else x
+                    val nextY = if (dir == 'N') i - 1 else if (dir == 'S') i + 1 else y
+                    val nextX = if (dir == 'W') i - 1 else if (dir == 'E') i + 1 else x
+                    if (tilted[nextY][nextX] == '.') {
+                        tilted[nextY][nextX] = 'O'
+                        tilted[currY][currX] = '.'
+                    } else {
+                        break
                     }
                 }
             }
         }
     }
-    return Pair(changed, tilted)
+    return tilted
 }
